4dd5ab145c23495add551d7589ece62d73219d9a
Siddesh N <siddeshsiddu049@gmail.com>
2026-01-15 17:21:20 +0530

    backend update

---
+++ latest/Forest-project/BackEnd/src/models/index.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database");
+
+const db = {};
+
+db.sequelize = sequelize;
+db.Sequelize = require("sequelize");
+
+// Import models
+db.VisitorBooking = require("./VisitorBooking");
+db.IndividualToken = require("./IndividualToken");
+db.Driver = require("./Driver");
+db.Vehicle = require("./Vehicle");
+db.VehicleAssignment = require("./VehicleAssignment");
+
+// Define associations
+db.VisitorBooking.hasMany(db.IndividualToken, {
+  foreignKey: "bookingId",
+  as: "individualTokens",
+});
+
+db.IndividualToken.belongsTo(db.VisitorBooking, {
+  foreignKey: "bookingId",
+  as: "booking",
+});
+
+module.exports = db;
